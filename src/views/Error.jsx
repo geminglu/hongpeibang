d07b4159223f8e43e566fc5830230c036449bc96
geminglu <18451487672@qq.com>
2020-04-25 19:29:32 +0800

    完善404

--- src/views/Error.jsx
+++ src/views/Error.jsx
@@ -1,21 +1,28 @@
 import React, {Component} from 'react'
-// import error from '../style/error.module.css'
-// import {Button} from 'react-bootstrap';
+import error from '../style/error.module.scss'
 
 export default class Error extends Component {
     render() {
-        return (<div></div>
-            // <div className={error.index}>
-            //     <img className={error.imdex_img} src={require('../images/404.png')} alt=""/>
-            //     <Button onClick={this.skip.bind(this)} style={{width: "150px",margin: "10px 20px"}} size="lg" variant="primary">返回首页</Button>
-            //     <Button onClick={this.go.bind(this)} style={{width: "150px",margin: "10px 20px"}} size="lg" variant="success">返回上一页</Button>
-            // </div>
+        return (
+            <div className={error.index}>
+                <img className={error.imdex_img} src={require('../images/404.png')} alt=""/>
+                <div className={error.button}>
+                    <div className={error.button_home} onClick={this.skip.bind(this)}
+                         style={{width: "150px", margin: "10px 20px"}}>返回首页
+                    </div>
+                    <div className={error.button_go} onClick={this.go.bind(this)}
+                         style={{width: "150px", margin: "10px 20px"}}>返回上一页
+                    </div>
+                </div>
+            </div>
         )
     };
-    skip(){
+
+    skip() {
         this.props.history.push("/")
     }
-    go(){
+
+    go() {
         this.props.history.go(-1)
     }
 }
