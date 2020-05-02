34d0b1ae289299093f26c1933f3df486347b59db
yys365 <3066001551@qq.com>
2020-05-02 19:26:13 +0800

    最新页面制作

--- src/views/Home/Circle/index.jsx
+++ src/views/Home/Circle/index.jsx
@@ -1,13 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import New from "./new";
+import Expert from "./expert";
+import { Switch, Route, Redirect, NavLink } from "react-router-dom"; //使用路由组建要引入
+import Topnav from "../../../components/Topnav";
+import Attention from "./attention";
 
 class Circle extends Component {
-    render() {
-        return (
-            <div>
-                烘培圈
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Topnav>
+          <div position="top_left">左</div>
+          <div position="top_rigth">右</div>
+          <div position="top_conent">中</div>
+        </Topnav>
+        <Route path={"/circle"} component={New}></Route>
+      </div>
+    );
+  }
 }
 
 export default Circle;
