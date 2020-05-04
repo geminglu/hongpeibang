111bae95b6e8550052c134e54978df48401af805
yys365 <3066001551@qq.com>
2020-05-04 10:18:52 +0800

    yemianwancheng

--- src/views/Home/Circle/index.jsx
+++ src/views/Home/Circle/index.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+//引入组件
 import New from "./new";
 import Expert from "./expert";
-import { Switch, Route, Redirect, NavLink } from "react-router-dom"; //使用路由组建要引入
-import Topnav from "../../../components/Topnav";
 import Attention from "./attention";
+
+import { Switch, Route, Redirect, NavLink } from "react-router-dom"; //使用路由组建要引入
+//引入头部公共组件
+import Topnav from "../../../components/Topnav";
+
+//引入scss样式
+import style from "./../../../style/home/circle/index.module.scss";
 
 class Circle extends Component {
   render() {
@@ -12,9 +18,25 @@ class Circle extends Component {
         <Topnav>
           <div position="top_left">左</div>
           <div position="top_rigth">右</div>
-          <div position="top_conent">中</div>
+          <div position="top_conent">
+            <NavLink
+              to={"/circle/attention"}
+              exact
+              activeClassName={style.active}
+            >
+              关注
+            </NavLink>
+            <NavLink to={"/circle"} exact activeClassName={style.active}>
+              最新
+            </NavLink>
+            <NavLink to={"/circle/expert"}  activeClassName={style.active}>
+              达人
+            </NavLink>
+          </div>
         </Topnav>
-        <Route path={"/circle"} component={New}></Route>
+        <Route path={"/circle/"} exact component={New}></Route>
+        <Route path={"/circle/expert"}  component={Expert}></Route>
+        <Route path={"/circle/attention"} exact component={Attention}></Route>
       </div>
     );
   }
--- src/views/Home/Circle/new.jsx
+++ src/views/Home/Circle/new.jsx
@@ -33,19 +33,94 @@ class New extends Component {
           </ul>
         </div>
         <div className={style.newin}>
-            <ul>
-                <li>#早餐#</li>
-                <li>#午餐#</li>
-                <li>#晚餐#</li>
-                <li>#下午茶#</li>
-                <li>#夜宵#</li>
-                <li>#ACA交作业#</li>
-                <li>#外貌协会#</li>
-                <li>#十多个地#</li>
-                <li>#撒的发生#</li>
-                <li>#好吃的早餐#</li>
-                
-            </ul>
+          <ul>
+            <li>#早餐#</li>
+            <li>#午餐#</li>
+            <li>#晚餐#</li>
+            <li>#下午茶#</li>
+            <li>#夜宵#</li>
+            <li>#ACA交作业#</li>
+            <li>#外貌协会#</li>
+            <li>#十多个地#</li>
+            <li>#撒的发生#</li>
+            <li>#好吃的早餐#</li>
+          </ul>
+        </div>
+        <div className={style.newmain}>
+          <ul>
+            <li>
+              <div className={style.newmain1}>
+                <div className={style.newmain1l}>
+                  <img src={require("../../../images/touxiang.png")} />
+                </div>
+                <div className={style.newmain1r}>
+                  <p>名字</p>
+                  <p>倒计时</p>
+                </div>
+              </div>
+              <div className={style.newmainc}>
+                输入水电费哈萨克缉毒风暴哈萨克打过比赛打输入法蒜蓉粉丝如果是的了卡机
+              </div>
+              <div className={style.newmainm}>
+                <img src={require("../../../images/one.jpg")} />
+              </div>
+              <div className={style.newmainf}>
+                <span>赞</span>
+
+                <span>打赏</span>
+
+                <span>评论</span>
+              </div>
+            </li>
+            <li>
+              <div className={style.newmain1}>
+                <div className={style.newmain1l}>
+                  <img src={require("../../../images/touxiang.png")} />
+                </div>
+                <div className={style.newmain1r}>
+                  <p>名字</p>
+                  <p>倒计时</p>
+                </div>
+              </div>
+              <div className={style.newmainc}>
+                输入水电费哈萨克缉毒风暴哈萨克打过比赛打输入法蒜蓉粉丝如果是的了卡机
+              </div>
+              <div className={style.newmainm}>
+                <img src={require("../../../images/one.jpg")} />
+              </div>
+              <div className={style.newmainf}>
+                <span>赞</span>
+
+                <span>打赏</span>
+
+                <span>评论</span>
+              </div>
+            </li>
+            <li>
+              <div className={style.newmain1}>
+                <div className={style.newmain1l}>
+                  <img src={require("../../../images/touxiang.png")} />
+                </div>
+                <div className={style.newmain1r}>
+                  <p>名字</p>
+                  <p>倒计时</p>
+                </div>
+              </div>
+              <div className={style.newmainc}>
+                输入水电费哈萨克缉毒风暴哈萨克打过比赛打输入法蒜蓉粉丝如果是的了卡机
+              </div>
+              <div className={style.newmainm}>
+                <img src={require("../../../images/one.jpg")} />
+              </div>
+              <div className={style.newmainf}>
+                <span>赞</span>
+
+                <span>打赏</span>
+
+                <span>评论</span>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     );
